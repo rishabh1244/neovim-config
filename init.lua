26d29ed20f7faf647368b21e7f008229db37549b
Rishabh K. <140598487+rishabh1244@users.noreply.github.com>
2025-11-02 17:05:16 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -3,5 +3,9 @@ require("rishabh.set")
 require("terminal")
 vim.opt.guifont = "Monocraft Nerd Font:h14"
 vim.opt.clipboard = "unnamedplus"
-
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    vim.g.airline_theme = 'onedark'
+  end,
+})
 ---31 10 25
